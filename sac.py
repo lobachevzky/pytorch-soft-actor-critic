5dd608303d8e86fff675fd1d8c59fb916bf7099d
Ethan Brooks <ethanabrooks@gmail.com>
2019-04-17 13:22:16 -0400

    plot policy loss coefficient

--- sac.py
+++ sac.py
@@ -262,6 +262,7 @@ class SAC(object):
         self.writer.add_scalar('policy loss', policy_loss.item(), updates)
         self.writer.add_scalar('Q', expected_new_q_value.mean().item(), updates)
         self.writer.add_scalar('reference log prob', reference_log_prob.mean().item(), updates)
+        self.writer.add_scalar('policy loss coefficient', coefficient, updates)
         self.writer.add_scalar('V', expected_value.mean().item(), updates)
         self.writer.add_scalar('Q1', expected_q1_value.mean().item(), updates)
         self.writer.add_scalar('Q2', expected_q2_value.mean().item(), updates)
